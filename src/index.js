c6af705790600fce6f9e8c780e2a70ca2eb1aaec
joao.oliveira0117@hotmail.com <joao.oliveira0117@hotmail.com>
2020-08-06 21:55:50 -0300

    add get, post, put and delete requests

--- src/index.js
+++ src/index.js
@@ -1,13 +1,76 @@
-//iniciar express e uuidv4
 const express = require("express");
 const {uuid} = require("uuidv4");
 const app = express();
 app.use(express.json());
 
-// criar o array de transações
-//começar pelo POST - a rota deve receber title, value, type, income/outcome.
-//fazer a lista do GET com todas os POSTs criados, somando um total com os valores(value) no final.
-//PUT-DELETE - edita, deleta - respectivamente.
+const transactions = [];
+
+app.get("/transactions", (request, response) => {
+    const { title, value, type } = request.query; 
+    var total = 0;
+    var income = 0;
+    var outcome = 0;
+    
+    for (transaction of transactions){
+        if(transaction.type.toLowerCase() == "income"){
+            total += transaction.value
+            income += transaction.value
+        }
+        else if(transaction.type.toLowerCase() == "outcome"){
+            total -= transaction.value
+            outcome += transaction.value 
+        }
+    }
+    
+    const balance = {income, outcome, total}
+    
+    return response.json({transactions, balance});
+});
+
+app.post('/transactions', (request, response) => {
+    const {title, value, type} = request.body;
+
+    const transaction = {id: uuid(),title, value, type};
+
+    transactions.push(transaction);
+
+    return response.json(transaction);
+});
+
+app.put("/transactions/:id", (request, response) => {
+    const { id } = request.params;
+    const { title, value, type } = request.body;
+  
+    const transactionIndex = transactions.findIndex((transaction) => transaction.id === id);
+  
+    const transaction = {
+      id,
+      title,
+      value,
+      type
+    };
+  
+    transactions[transactionIndex] = transaction;
+  
+    return response.json(transactions);
+  });
+
+
+app.delete('/transactions/:id', (request, response) => {
+
+    const { id } = request.params;
+    const { title, value, type } = request.params;
+    const transactionIndex = transactions.findIndex(transaction => transaction.id == id);
+    
+    if(transactionIndex < 0) {
+        return response.status(400).json({error: "Transaction not found"});
+    }
+
+    transactions.splice(transactionIndex, 1);
+
+    return response.status(204).send();
+})
+
 //iniciar MIDDLEWARES.
 
 //iniciar a porta do insomnia
